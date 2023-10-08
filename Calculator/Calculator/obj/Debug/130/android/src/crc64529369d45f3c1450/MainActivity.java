baa6593620785c6abb1967259c6004dfdf7bc81a
Admin <Admin@DESKTOP-H1E17TT>
2023-10-08 17:31:35 +0700

    +-=/ Done

--- Calculator/Calculator/obj/Debug/130/android/src/crc64529369d45f3c1450/MainActivity.java
+++ Calculator/Calculator/obj/Debug/130/android/src/crc64529369d45f3c1450/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity
 		__md_methods = 
 			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
 			"n_onRequestPermissionsResult:(I[Ljava/lang/String;[I)V:GetOnRequestPermissionsResult_IarrayLjava_lang_String_arrayIHandler\n" +
+			"n_ButtonClick:(Landroid/view/View;)V:__export__\n" +
 			"";
 		mono.android.Runtime.register ("Calculator.MainActivity, Calculator", MainActivity.class, __md_methods);
 	}
@@ -49,6 +50,14 @@ public class MainActivity
 	}
 
 	private native void n_onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2);
+
+
+	public void ButtonClick (android.view.View p0)
+	{
+		n_ButtonClick (p0);
+	}
+
+	private native void n_ButtonClick (android.view.View p0);
 
 	private java.util.ArrayList refList;
 	public void monodroidAddReference (java.lang.Object obj)
